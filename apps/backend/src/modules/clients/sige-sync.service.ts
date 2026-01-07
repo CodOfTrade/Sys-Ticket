534c78554e1dc73f107c2f91c74d5b1dc44befe0
CodOfTrade <jose.tecnoinf@gmail.com>
2026-01-07 14:14:03 -0300

    Fix: reverter endpoint de contratos para Pesquisar (GetAll não existe)

--- apps/backend/src/modules/clients/sige-sync.service.ts
+++ apps/backend/src/modules/clients/sige-sync.service.ts
@@ -125,7 +125,7 @@ export class SigeSyncService {
       let totalSynced = 0;
 
       while (hasMore) {
-        const rawResponse = await this.sigeCloudService.get<any>('/request/Contratos/GetAll', {
+        const rawResponse = await this.sigeCloudService.get<any>('/request/Contratos/Pesquisar', {
           pageSize: this.BATCH_SIZE,
           skip: (page - 1) * this.BATCH_SIZE,
         });
